21f556cf2455fc559eb0b4756832dcba95147e17
monitorius <monitorius@yandex-team.ru>
2014-01-10 12:06:14 +0600

    fix selector; includes

--- github_pullrequest_fold_file/github_pullrequest_fold_file.user.js
+++ github_pullrequest_fold_file/github_pullrequest_fold_file.user.js
@@ -2,12 +2,16 @@
 // @name        github pullrequest fold file
 // @namespace   my_github
 // @include     https://github.yandex-team.ru/*/*/pull/*
+// @include     https://github.yandex-team.ru/*/*/commit/*
+// @include     https://github.yandex-team.ru/*/*/compare/*
 // @include     https://github.com/*/*/pull/*
+// @include     https://github.com/*/*/commit/*
+// @include     https://github.com/*/*/compare/*
 // @version     1
 // @grant       none
 // ==/UserScript==
 
 
-$('.diff-view.commentable .file.js-details-container .meta').on('click', function() {
+$('.diff-view .file.js-details-container .meta').on('click', function() {
 	$(this).next().toggle();
 });
